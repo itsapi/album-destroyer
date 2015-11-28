2b100f2e3e629b7480f43394f76f69dba9decbbe
Oliver Faircliff <mail@oliverfaircliff.com>
2015-11-28 16:37:47 +0000

    Play WAV files

---
+++ play.py
@@ -0,0 +1,30 @@
+import pyaudio
+import wave
+import sys
+
+
+def play_wave(filename):
+    CHUNK = 1024
+
+    wf = wave.open(filename, 'rb')
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(CHUNK)
+
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
+
+
+if __name__ == '__main__':
+    play_wave(sys.argv[1])
