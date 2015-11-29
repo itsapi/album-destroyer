b45699f73525e7494cff608f1ed7ab4c214d7cc0
Geraint White <geraintwhite@gmail.com>
2015-11-29 06:15:27 +0000

    Add lastfm username env variable

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 from time import sleep
 from queue import Queue
 from difflib import SequenceMatcher as SM
+from os import getenv
 
 import lastfm
 import convert_image
@@ -64,7 +65,7 @@ def scroll_image(image, offset):
 
 
 def main():
-    albums = lastfm.get_albums('ollsllo')
+    albums = lastfm.get_albums(getenv('username'))
     offset = HEIGHT
 
     answer = Input(HEIGHT - 1, 0)
